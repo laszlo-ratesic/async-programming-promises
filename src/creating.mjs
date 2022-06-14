4d0d1550e48ee8f851d3c41a30ca747b317bad9d
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-14 13:54:23 -0500

    observe effects of resolved interval

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,29 +1,34 @@
-import setText, { appendText } from "./results.mjs";
+import setText, { appendText } from './results.mjs';
 
 export function timeout() {
-    const wait = new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Timeout!");
-        }, 1500);
-    });
+  const wait = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Timeout!');
+    }, 1500);
+  });
 
-    wait.then(text => setText(text));
+  wait.then((text) => setText(text));
 }
 
 export function interval() {
+  let counter = 0;
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      console.log('interval');
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+
+  wait.then((text) => setText(text))
+  .finally(() => appendText(` -- Done ${counter}`));
 }
 
-export function clearIntervalChain() {
-}
+export function clearIntervalChain() {}
 
-export function xhr() {
-}
+export function xhr() {}
 
-export function allPromises() {
-}
+export function allPromises() {}
 
-export function allSettled() {
-}
+export function allSettled() {}
 
-export function race() {
-}
+export function race() {}
