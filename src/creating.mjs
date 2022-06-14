4d44b4cfd45dd0db413540b8d8000c6ac4790d1e
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-06-14 14:05:50 -0500

    handling promise states

--- src/creating.mjs
+++ src/creating.mjs
@@ -37,7 +37,23 @@ export function clearIntervalChain() {
   wait.then((text) => setText(text)).finally(() => clearInterval(interval));
 }
 
-export function xhr() {}
+export function xhr() {
+  let request = new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/users/7');
+    xhr.onload = () => {
+        if(xhr.status === 200) {
+            resolve(xhr.responseText);
+        } else {
+            reject("Request Failed");
+        }
+    }
+    xhr.onerror = () => reject('Network error');
+    xhr.send();
+  });
+
+  request.then((result) => setText(result)).catch((reason) => setText(reason));
+}
 
 export function allPromises() {}
 
